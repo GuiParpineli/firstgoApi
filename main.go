b0b5d0079f8b2d21cdeeeb7caedea0ff3fb2b43c
Gui Parpineli <guiparpineli@GuiPC>
2023-01-17 17:47:34 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"apiGin/controllers"
+	"apiGin/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
